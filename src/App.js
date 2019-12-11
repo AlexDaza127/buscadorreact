e8c5967759136e1db9a11d65be4ae512df2f7c06
Michael <Michael@LAPTOP-2DE4KAKH>
2019-12-11 16:21:24 -0500

    Este metodo permite obtener los datos del componente impreso desde app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Buscador from './componentes/Buscador';
 
 
 class App extends Component {
+
+  datosBusqueda = (termino) => {
+    console.log(termino);
+  }
+
+
   render(){
     return (
       <div className="app container">
         <div className="jumbotron">
           <p className="lead text-center">Buscador de imagenes</p>
           {/*Utilizando el componente Buscador para lanzarlo en la aplicación*/}
-          <Buscador></Buscador>
+          <Buscador  datosBusqueda={this.datosBusqueda}>
+           
+          </Buscador>
         </div>
       </div>
     );
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 
 class Buscador extends Component {
-    state = {
+    busquedaRef = React.createRef();
 
+/* Este metodo permite obtener los datos del componente impreso en pantala */
+    obtenerDatos =(e) => {
+        e.preventDefault();
+        console.log(this.busquedaRef.current.value);
+        
     }
-    render() {
+
+    render(){
         return (
-            <form>
+            <form onSubmit={this.obtenerDatos}>
                 <div className="row">
+                    {/*Codigo de la barra de busqueda*/}
                     <div className="form-group col-md-8">
-                        <input type="text" className="form-control for-control-lg" 
-                        placeholder="Busca tu Imagen. Ejemplo: Furbol"></input>
+                        <input ref={this.busquedaRef} type="text" className="form-control for-control-lg" 
+                        placeholder="Busca tu Imagen. Ejemplo: Futbol"></input>
                     </div>
+                    {/*Codigo del boton de busqueda*/}
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-lg btn-danger btn-block"
-                        placeholder="Busca tu Imagen"></input>
+                        placeholder="Busca tu Imagen"
+                        value="Buscar...">
+                        </input>
                     </div>
                 </div>
                
