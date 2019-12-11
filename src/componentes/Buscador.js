fb50487457ebdf5d8b55d2be264c092a72108bb8
Michael <Michael@LAPTOP-2DE4KAKH>
2019-12-11 16:23:38 -0500

    Este metodo permite obtener los datos del componente impreso desde app.js

--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -3,31 +3,30 @@ import React, { Component } from 'react'
 class Buscador extends Component {
     busquedaRef = React.createRef();
 
-/* Este metodo permite obtener los datos del componente impreso en pantala */
-    obtenerDatos =(e) => {
+    /* Este metodo permite obtener los datos del componente impreso desde app.js */
+    obtenerDatos = (e) => {
         e.preventDefault();
         console.log(this.busquedaRef.current.value);
-        
     }
 
-    render(){
+    render() {
         return (
             <form onSubmit={this.obtenerDatos}>
                 <div className="row">
                     {/*Codigo de la barra de busqueda*/}
                     <div className="form-group col-md-8">
-                        <input ref={this.busquedaRef} type="text" className="form-control for-control-lg" 
-                        placeholder="Busca tu Imagen. Ejemplo: Futbol"></input>
+                        <input ref={this.busquedaRef} type="text" className="form-control for-control-lg"
+                            placeholder="Busca tu Imagen. Ejemplo: Futbol"></input>
                     </div>
                     {/*Codigo del boton de busqueda*/}
                     <div className="form-group col-md-4">
                         <input type="submit" className="btn btn-lg btn-danger btn-block"
-                        placeholder="Busca tu Imagen"
-                        value="Buscar...">
+                            placeholder="Busca tu Imagen"
+                            value="Buscar...">
                         </input>
                     </div>
                 </div>
-               
+
             </form>
         );
     }
