de0cdd0632bf607dacc4d0cb3114b891b3b76db9
Michael <Michael@LAPTOP-2DE4KAKH>
2019-12-16 15:12:23 -0500

    fetch api con la url de la api de pixabay

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import Buscador from './componentes/Buscador';
 
 class App extends Component {
 
+state = {
+  termino : 'Desarrollo'
+
+}
+
+//se utiliza un fetch api con la url de la api de pixabay
+consultarApi = () => {
+  const url = 'https://pixabay.com/api/?key=1732750-d45b5378879d1e877cd1d35a6&q=${this.state.termino}';
+  console.log(url);
+}
+
   //metodo que recibe el parametro recibido del input
   datosBusqueda = (termino) => {
     //se imprime lo recibido en la consola
-    console.log(termino);
+    this.setState({
+      termino
+    })
   }
 
 
@@ -21,6 +34,7 @@ class App extends Component {
            
           </Buscador>
         </div>
+        {this.state.termino}
       </div>
     );
   }
